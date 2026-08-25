55f6ee0944e54a0a8d373b263c22e4c43b3e4abf
susukinokenwa <118701401+susukinokenwa@users.noreply.github.com>
2026-08-25 15:51:38 +0800

    1. update boxes 2.  update pen color 3. added erase option

--- main.js
+++ main.js
@@ -1,11 +1,59 @@
 const container = document.querySelector(".container");
 
+const height = 960;
+const width = 960;
+let erase = false
 
 let box = document.createElement("div");
 box.classList.add("box");
-for(let i = 0; i < 16; i++){
-    let box = document.createElement("div");
-box.classList.add("box");
-    console.log("ji");
-    container.appendChild(box);
+
+
+function addboxes(boxcount){
+    container.replaceChildren();
+    let boxWidth = 960/boxcount;
+    for(let i = 0; i < boxcount; i++){
+        for(let j = 0; j < boxcount; j++)
+        {
+            let box = document.createElement("div");
+            box.classList.add("box");
+            box.style.width = `${boxWidth}px`;
+            box.style.height = `${boxWidth}px`;
+            box.addEventListener('mouseenter', () => {
+                if (erase) box.style.backgroundColor = "black";
+                else randomColor(box);
+            });
+            
+            container.appendChild(box);
+        }
+    }
 }
+
+function randomColor(box){
+    let red = Math.floor(Math.random()*1000 % 41);
+    let green = Math.floor(Math.random()*1000 % 81)+40;
+    let blue = Math.floor(Math.random()*1000 % 81)+100;
+    box.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+}
+    
+    
+
+
+//call once for defulat
+addboxes(16);
+
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", () => {
+    let boxcount = prompt("How many boxes per side do you want? (1-100)", 16);
+    addboxes(boxcount);
+});
+
+let erasebtn = document.querySelector(".btn2");
+erasebtn.addEventListener("click", () =>{
+    if(erase) {
+        erase = false;
+        erasebtn.textContent = "ERASE: off";
+    }else {
+        erase = true;
+        erasebtn.textContent = "ERASE: on";
+    }
+});
